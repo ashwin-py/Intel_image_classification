95a987801f178e90229e2cff9fbf92183e006c8b
ashwin <71367090+geraltPy@users.noreply.github.com>
2021-09-17 18:00:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
-from flask import Flask, request, render_template
+#importing necessary libraries
 import os
+from flask import Flask, request, render_template
 from flask_cors import CORS, cross_origin
 from classifier import Classifier
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
+# Path to the model
 model_path = "./mobilenetv3"
 
 app = Flask(__name__)
 CORS(app)
 classifier = Classifier(model=model_path)
 
-
+# Function for rednering the Home HTML page
 @app.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
 
-
+# Function for predicting the class of submitted image
 @app.route("/submit", methods=['GET', 'POST'])
 @cross_origin()
 def predict():
